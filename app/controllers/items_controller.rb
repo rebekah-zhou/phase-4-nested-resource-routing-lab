2f1cf610b8567c265912611d1dad6b7992c06156
Rebekah Zhou <rebekahzhou@gmail.com>
2022-03-20 22:26:27 -0700

    commit tree 7eca056220c04cadd515004328d8dac2c8c95095

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_msg
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id] 
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create 
+    item = Item.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_msg
+    render json: { error: "Item not found" }, status: :not_found
   end
 
 end
